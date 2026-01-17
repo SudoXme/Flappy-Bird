7b619b20dcc317b2acc58d7895a1e6a1dc0bcf14
SudoXme <consoleloglifex@gmail.com>
2026-01-17 11:24:40 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+
+export default function landingPage () {
+  return <div>
+    <h1>This is the landing page</h1>
+    <h2>Lets configure Tailwind css</h2>
+    <h1>This is a new div</h1>
+  </div>
+}
