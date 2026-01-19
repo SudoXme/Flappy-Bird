4a84f297cbaa045eb42b81c8312f112fc36bfef6
SudoXme <consoleloglifex@gmail.com>
2026-01-19 06:36:11 +0000

    Configure Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 export default function landingPage () {
   return <div>
-    <h1>This is the landing page</h1>
+    <h1 className="text-blue-500">This is the landing page</h1>
     <h2>Lets configure Tailwind css</h2>
     <h1>This is a new div</h1>
   </div>
