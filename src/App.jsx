34a8af81c2018499fa9466766c90723bd6899f64
SudoXme <consoleloglifex@gmail.com>
2026-02-03 08:51:19 +0000

    Add Jersey font

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,21 @@ import bird from "./assets/bird.png"
 
 export default function landingPage () {
 {
-    // So the first thing that is going to be done is the background image, that will me modified so that the background image will only have the image of a sky.
     // Now lets add the concept of pipes.
     // I will be needing pipes of different lengths.
     // The size of those pipes should be small
     // The concept behind pipes of different length will be I have to write code to include the pipes of different lengths.
+    // The next step is going to be the usage of pixel family
+    // I have to write a script.js file for the usage of pixelated fonts
+    // I will export the fonts from that script and import in this page
   }
-  return <div className="relative font-mono">
+  return <div className="relative font-jersey">
     <img src={sky} className="h-screen w-screen"/>
     <img src={ground} className="absolute bottom-0"/>
     <img src={logo} className="absolute top-20 left-130"/>
     <img src={pipe} className="absolute bottom-39 right-40" />
     <img src={invertedPipe} className="absolute top-0 left-40"/>
     <img src={bird} className="absolute top-60 right-170"/>
-    <button className="absolute top-90 right-155 text-4xl text-[#F3FEA7] bg-[#9EEB59] border-red-500 rounded-sm font-extrabold cursor-pointer px-3 py-4 hover:bg-red-400">Start Game</button>
+    <button className="absolute top-90 right-140 text-6xl text-[#2F3B16] bg-[#9EEB59] outline-solid outline-black outline-4 rounded-sm font-extrabold cursor-pointer px-3 py-4 hover:bg-red-400 shadow-lg ">START GAME</button>
   </div>
 }
