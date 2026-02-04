cc9102d15e8223fbaff26085aae655b1cd2e947e
SudoXme <consoleloglifex@gmail.com>
2026-02-04 07:21:55 +0000

    Remove outdated comments

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,6 @@ import invertedPipe from "./assets/inverted pipe.png"
 import bird from "./assets/bird.png"
 
 export default function landingPage () {
-{
-    // Now lets add the concept of pipes.
-    // I will be needing pipes of different lengths.
-    // The size of those pipes should be small
-    // The concept behind pipes of different length will be I have to write code to include the pipes of different lengths.
-    // The next step is going to be the usage of pixel family
-    // I have to write a script.js file for the usage of pixelated fonts
-    // I will export the fonts from that script and import in this page
-  }
   return <div className="relative font-jersey">
     <img src={sky} className="h-screen w-screen"/>
     <img src={ground} className="absolute bottom-0"/>
